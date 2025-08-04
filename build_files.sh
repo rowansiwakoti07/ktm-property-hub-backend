9440251d58035a36a6c895db8e5a2eb2144ab3ce
Dikendra Siwakoti <dikendra.siwakoti@verisk.com>
2025-08-04 14:54:38 +0545

    asdfasdf

--- build_files.sh
+++ build_files.sh
@@ -10,3 +10,6 @@ python3 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
+
+echo "Collecting Static..."
+python3 manage.py collectstatic --noinput
