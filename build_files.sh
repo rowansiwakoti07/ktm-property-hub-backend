47c54083410d4f936fb5da5e7b1e5a58e4c40a67
Dikendra Siwakoti <dikendra.siwakoti@verisk.com>
2025-08-04 14:33:25 +0545

    Remove collect static

--- build_files.sh
+++ build_files.sh
@@ -10,6 +10,3 @@ python3 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3 manage.py collectstatic --noinput --clear
